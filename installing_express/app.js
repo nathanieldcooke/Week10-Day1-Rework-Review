f4fad21d6e01534669ad2e31d87f8b1935411a89
nathanieldcooke <nathanield.cooke@gmail.com>
2021-10-17 08:22:26 -0700

    complete Practice: Intro To Express

--- installing_express/app.js
+++ installing_express/app.js
@@ -1,3 +1,11 @@
 const express = require('express')
 
 const app = express()
+
+const port = 5000;
+
+app.get('/status', (req, res) => {
+    res.send('The server is alive!')
+})
+
+app.listen(port, () => console.log(`Server is listening on Port: ${port}`))
